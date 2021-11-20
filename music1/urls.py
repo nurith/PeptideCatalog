87ad8e87f995efdaf33bee29f30e477e856250c9
nurith <nurit.haspel@umb.edu>
2021-11-20 17:51:29 -0500

    Modified urls.py

--- music1/urls.py
+++ music1/urls.py
@@ -15,15 +15,16 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^$', music1_views.index, name='index'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^search/',music1_views.SearchView.as_view(), name='search'),
     url(r'^setrow/(?P<row>\w+)/$',music1_views.ProteinTableView.as_view(), name='setrow'),
     url(r'^database/$',music1_views.ProteinTableView.as_view(), name='database'),
     url(r'^contact/$',music1_views.contact, name='contact'),
     url(r'^contributors/$',music1_views.contrib, name='contributors'),
-    url(r'^database/entry/(?P<entry>\w+)/$',music1_views.entry, name='detail'),
+    url(r'^database/entry/(?P<entry>\w+)/$',music1_views.entry, name='entry'),
     url(r'^fasta/(?P<seq>\w+)/$', music1_views.sequence, name='sequence'),
     url(r'^uniprot/(?P<uni>\w+)/$', music1_views.ViewUni.as_view(), name='uniprot_redirect'),
     url(r'^peptidedb/(?P<pepdb>\w+)/$', music1_views.ViewPeptideDB.as_view(), name='peptidedb_redirect'),
     url(r'^endonet/(?P<endo>\w+)/$', music1_views.ViewEndo.as_view(), name='endonet_redirect'),
+    url(r'^list/$', music1_views.filt_list),
 ]
